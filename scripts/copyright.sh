c5f236cb145c8210aaf81593a1a882d65f2ea511
Vasili Gulevich <vasili.gulevich@xored.com>
2025-04-05 16:03:24 +0400

    Remove redundant spaces from copyright header

--- scripts/copyright.sh
+++ scripts/copyright.sh
@@ -50,10 +50,10 @@ cat > "$HEADER_FILE" <<EOF
 # This program and the accompanying materials are made available
 # under the terms of the Eclipse Public License 2.0 which is available
 # at http://www.eclipse.org/legal/epl-2.0.
-# 
+#
 # SPDX-License-Identifier: EPL-2.0
-# 
-# Contributors: 
+#
+# Contributors:
 #    Xored Software Inc. - initial API and implementation
 ###############################################################################
 EOF
