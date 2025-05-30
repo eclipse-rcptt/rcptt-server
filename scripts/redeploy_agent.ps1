75c58f99108ba1d911457734a5ab79c9267b29af
Vasili Gulevich <vasili.gulevich@xored.com>
2025-05-30 20:26:37 +0400

    Update redeployment scripts

--- scripts/redeploy_agent.ps1
+++ scripts/redeploy_agent.ps1
@@ -15,4 +15,4 @@ if (Test-Path agent) {
 	Remove-Item -Recurse -Force agent -ErrorAction Stop
 }
 Expand-Archive -Path "org.eclipse.rcptt.cloud.agent.product-win32.win32.x86_64.zip" -DestinationPath "agent"
-Start-Process -NoNewWindow agent\eclipsec.exe -ArgumentList "-consoleLog -serverHost 172.29.1.229  -serverPort 5009 -data agent\ws -vm `"C:\Users\Test\Documents\jdk-21.0.6.7-hotspot\bin\java.exe`" -vmargs -XX:+HeapDumpOnOutOfMemoryError -Xlog:gc"
+Start-Process -Wait agent\eclipsec.exe -ArgumentList "-consoleLog -serverHost 172.29.1.229  -serverPort 5009 -data agent\ws -vm `"C:\Users\Test\Documents\jdk-21.0.6.7-hotspot\bin\java.exe`" -vmargs -XX:+HeapDumpOnOutOfMemoryError -Xlog:gc"
--- scripts/redeploy_remote_windows_agent.sh
+++ scripts/redeploy_remote_windows_agent.sh
@@ -10,5 +10,8 @@ remote_sum=`ssh "$AGENT_HOST" 'certUtil -hashfile %HOME%/Documents/org.eclipse.r
 local_sum=`shasum -b -a 512  "$DIST" | cut -d ' ' -f 1` 
 if [[ $remote_sum != $local_sum ]] ; then
 	scp "$DIST" "$AGENT_HOST":Documents/
-fi  
-ssh -tt "$AGENT_HOST" "powershell.exe -NonInteractive  %HOME%/Documents/redeploy_agent.ps1" &
+fi
+#ssh "$AGENT_HOST" 'taskkill.exe /F /IM eclipsec.exe'
+# ssh "$AGENT_HOST" 'taskkill.exe /F /IM java.exe'
+# To start agent with GUI, run redeploy_agent.ps1 in GUI context
+#ssh -tt "$AGENT_HOST" "powershell.exe -NonInteractive  %HOME%/Documents/redeploy_agent.ps1" &
