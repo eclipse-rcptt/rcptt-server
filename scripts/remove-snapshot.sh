92702d54ceb7341c879a34beaca2649f546afc96
Vasili Gulevich <vasili.gulevich@xored.com>
2025-07-10 23:55:28 +0400

    Fix typo

--- scripts/remove-snapshot.sh
+++ scripts/remove-snapshot.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SNAPSHOT_VERSION=`${MVN_CMD} help:evaluate -q --non-recursive -Dtycho.mode=maven -DforceStdout=true -Dexpression='project.version'`
+SNAPSHOT_VERSION=`${MVN_CMD=mvn} help:evaluate -q --non-recursive -Dtycho.mode=maven -DforceStdout=true -Dexpression='project.version'`
 RELEASE_VERSION=${SNAPSHOT_VERSION%-SNAPSHOT}
 
 `dirname $0`/set-version.sh "$RELEASE_VERSION"
