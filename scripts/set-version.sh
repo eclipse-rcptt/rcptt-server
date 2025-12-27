f27046edb5716fc373cf09588c96df2b326442a7
Vasili Gulevich <vasili.gulevich@xored.com>
2025-12-27 23:52:41 +0400

    System test prototype #35

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,3 +1,5 @@
 #/bin/sh 
 set -ex
 ${MVN_CMD=mvn} -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin::set-version "-DnewVersion=$1"
+${MVN_CMD=mvn} -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin::set-version "-DnewVersion=$1" --file rcpttTests
+${MVN_CMD=mvn} -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin::set-property rcptt-maven-version "$1"
